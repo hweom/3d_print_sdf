2ccdebc683aff18487fd817af85f29368871d784
Mikhail Balakhno <{ID}+{username}@users.noreply.github.com>
2025-03-22 12:07:05 -0700

    More Bosch slatwall mounts

--- slatwall_mount/README.md
+++ slatwall_mount/README.md
@@ -10,12 +10,31 @@ I recommend to print them on the side so that tension forces will work along fil
 
 ![slicing](example_slicing.png)
 
+Some holders can be print as one piece (slatwall clamp and the mount), some have to be splitted into the clamp and a mount which can be connected together.
+
+## Universal clamp
+
+![clamp](preview_clamp.png)
+
+
 ## Bosch 12V powertool holders
 
-Two variations, one that is more suitable for drills, screwdrivers etc:
+Charger mount:
+
+![bosch12v_charger](preview_bosch12_charger_mount.png)
+
+Tool holders. Two variations, one that is more suitable for drills, screwdrivers etc:
 
 ![bosch12v](preview_bosch12v.png)
 
 and the other for longer and narrower tools (rotary tool etc):
 
 ![bosch12v_vertical](preview_bosch12v_holder_vertical.png)
+
+Battery holder. This needs to be paired with the universal clamp.
+
+![bosch12v_battery](preview_bosch12v_battery_holder.png)
+
+Here how it looks in practice:
+
+![bosch](example_bosch.jpg)
--- slatwall_mount/parts.py
+++ slatwall_mount/parts.py
@@ -8,11 +8,17 @@ SLAT_LATCH_HEIGHT = 10
 SLAT_LATCH_DEPTH = 5
 
 PLATE_THICK = 6
-PLATE_WIDTH = 40
+PLATE_WIDTH = 39
 PLATE_HEIGHT = SLAT_PANEL_HEIGHT + PLATE_THICK * 2
 
 HOOK_WIDTH = 15
 HOOK_FUDGE = 1.5  # Fudge factor for tension
+
+PIN_RAD = 2.5
+PIN_HEIGHT = PLATE_THICK
+
+CONNECTOR_INLAY_SIZE = 5
+CONNECTOR_TOLERANCE = 0.5
 
 # Base mounting platform.
 def create_platform(width):
@@ -38,9 +44,24 @@ def create_platform(width):
 
     return mount
 
+def create_connector_template(width, enlarge_by = 0):
+    bar = box((width, CONNECTOR_INLAY_SIZE + enlarge_by, CONNECTOR_INLAY_SIZE + enlarge_by)).rotate(pi/4, X)
+    
+    # Add a little bump in the middle to fixate the connection.
+    bar = bar | box((0.01, CONNECTOR_INLAY_SIZE + enlarge_by, CONNECTOR_INLAY_SIZE + enlarge_by)).dilate(CONNECTOR_TOLERANCE).rotate(pi/4, X)
+    
+    return bar & slab(z1 = CONNECTOR_INLAY_SIZE/2)
+
+# Base platfrom with a hole and a mating pin for connecting other parts.
+def create_platform_with_connectors(width):
+    platform = create_platform(width)
+
+    return platform | create_connector_template(width).translate((width/2, PLATE_HEIGHT/2, -CONNECTOR_INLAY_SIZE/2))
+
+
 # Mount for Bosch 12V powertools.
 def create_bosch12v_tool_holder():
-    platform_width = 39
+    PLATE_WIDTH = 39
 
     # Triangular column with rounded edges, rotated.
     insert_template = equilateral_triangle().extrude(16).dilate(1).rotate(pi, Z).rotate(pi/6, X)
@@ -49,16 +70,15 @@ def create_bosch12v_tool_holder():
     insert = insert_template.scale(9.75)
 
     # Add the outer shape of the insert to the platform (cutting the part that would stick out from behind before that).
-    holder = create_platform(platform_width) | (insert.translate((platform_width/2, PLATE_HEIGHT/2, 0)) & slab(z1=1))
+    holder = create_platform(PLATE_WIDTH) | (insert.translate((PLATE_WIDTH/2, PLATE_HEIGHT/2, 0)) & slab(z1=1))
 
     # Subtract smaller insert shape to make it hollow.
-    holder = holder - insert_template.scale(7).translate((platform_width/2, PLATE_HEIGHT/2, 0))
+    holder = holder - insert_template.scale(7).translate((PLATE_WIDTH/2, PLATE_HEIGHT/2, 0))
     
     return holder
 
 # Vertical mount for Bosch 12V powertools.
 def create_bosch12v_tool_vertical_holder():
-    platform_width = 39
     ledge_depth = 20
 
     # Triangular column with rounded edges, rotated.
@@ -68,14 +88,57 @@ def create_bosch12v_tool_vertical_holder():
     insert = insert_template.scale(9.75)
 
     # Add the outer shape of the insert to the platform (cutting the part that would stick out from behind before that).
-    holder = create_platform(platform_width)
-    holder = holder | box((platform_width, PLATE_THICK*2, platform_width)).translate((platform_width/2, 30, -platform_width/2))
-    holder = holder | (insert.translate((platform_width/2, PLATE_HEIGHT/2, -20)) & slab(y0=30))
+    holder = create_platform(PLATE_WIDTH)
+    holder = holder | box((PLATE_WIDTH, PLATE_THICK*2, PLATE_WIDTH)).translate((PLATE_WIDTH/2, 30, -PLATE_WIDTH/2))
+    holder = holder | (insert.translate((PLATE_WIDTH/2, PLATE_HEIGHT/2, -20)) & slab(y0=30))
 
     # Subtract smaller insert shape to make it hollow.
-    holder = holder - (insert_template.scale(7).translate((platform_width/2, PLATE_HEIGHT/2, -20)) & slab(z1=0))
+    holder = holder - (insert_template.scale(7).translate((PLATE_WIDTH/2, PLATE_HEIGHT/2, -20)) & slab(z1=0))
     
     return holder
+
+# Mount for the Bosch 12V charger.
+def create_bosch12v_charger_mount():
+    screw_rad = 2
+    screw_head_recess_rad = 8
+    screw_head_recess_depth = 4
+
+    mount = create_platform(PLATE_WIDTH)
+
+    screw_hole_offset = (PLATE_WIDTH/2, PLATE_HEIGHT*3/4, 0)
+
+    # Through hole for the screw.
+    mount = mount - cylinder(screw_rad).translate(screw_hole_offset)
+
+    # Recess for the screw head.
+    mount = mount - (cylinder(screw_head_recess_rad).translate(screw_hole_offset) - slab(z1=(PLATE_THICK - screw_head_recess_depth)))
+
+    return mount
+
+def create_bosch12v_battery_holder():
+    template_scale_factor = 13.2
+    base_thick = CONNECTOR_INLAY_SIZE + 2
+    sleeve_height = 50
+
+    # Triangular column with rounded edges, rotated.
+    template = equilateral_triangle().extrude(10).dilate(1).rotate(pi, Z)#.rotate(pi*2/5, X)
+
+    sleeve = template.scale(template_scale_factor) #- template.scale(template_scale_factor * 0.8)
+    sleeve = sleeve & slab(z0=-sleeve_height)
+
+    holder = sleeve.rotate(pi/6, X) & slab(z1=0)
+
+    # Add ridges to the cutout template to create grooves in the sleeve.
+    cutout = template | (rectangle(2.6).extrude(10).translate((0, -0.2, 0)))
+
+    holder = holder - ((cutout & slab(z1 = -1)).scale(template_scale_factor * 0.8).rotate(pi/6, X) )
+
+    # Subtract the connector template to create a female connection point to the base.
+    connector_width = 100  # Can be bigger since we're subtracting it.
+    holder = holder - create_connector_template(connector_width, CONNECTOR_TOLERANCE).translate((0, 0, -CONNECTOR_INLAY_SIZE/2))
+
+    return holder
+
 
 
 QUALITY = 25
@@ -84,5 +147,8 @@ QUALITY = 25
 # Uncomment the parts that need to be generated below.
 #
 
+# create_platform_with_connectors(PLATE_WIDTH).save('platform.stl', samples = 2**QUALITY)
 # create_bosch12v_tool_holder().save('bosch12v_tool_holder.stl', samples = 2**QUALITY)
 # create_bosch12v_tool_vertical_holder().save('bosch12v_tool_vertical_holder.stl', samples = 2**QUALITY)
+# create_bosch12v_charger_mount().save('bosch12v_charger_mount.stl', samples = 2**QUALITY)
+# create_bosch12v_battery_holder().save('bosch12v_battery_holder.stl', samples = 2**QUALITY)
